36e72942508b7dcf3d52979c86cfa50fbe94e775
Cadenza <mirandalucsb@gmail.com>
2022-01-18 18:01:40 -0700

    added CRUD functions

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const path = require("path");
 const express = require("express");
 const app = express();
+const {notes} = require('../db/db');
+const { filterByQuery, makeNewNote, validateNote, updateNote, deleteNote} = require('../lib/notes');
 
 const PORT = process.env.PORT || 3001;
 
@@ -13,14 +15,19 @@ const PORT = process.env.PORT || 3001;
 
 
  app.get('/notes', (req, res) => {
-    //return notes here
+    //return notes here - filterByQuery
     res.send("hello world!");
  }) 
  //need a post route for posting new notes
+ //need a route for deleting notes
 
  app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
  })
+
+ app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
 
   app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
