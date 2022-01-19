983e4a66a976fa89658477a52a545c7cc6eab025
Cadenza <mirandalucsb@gmail.com>
2022-01-19 16:55:34 -0700

    tested get and post routes for notes

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 const path = require("path");
 const express = require("express");
 const app = express();
-const {notes} = require('../db/db');
-const { filterByQuery, makeNewNote, validateNote, updateNote, deleteNote} = require('../lib/notes');
+const notes = require('./db/db.json');
+const { filterByQuery, makeNewNote, validateNote, updateNote, deleteNote} = require('./lib/notes');
 
 const PORT = process.env.PORT || 3001;
 
@@ -14,20 +14,39 @@ const PORT = process.env.PORT || 3001;
   app.use(express.static('public'));
 
 
- app.get('/notes', (req, res) => {
+ app.get('/api/notes', (req, res) => {
     //return notes here - filterByQuery
-    res.send("hello world!");
+    let filteredNotes = notes;
+    if(req.query){
+      filteredNotes = filterByQuery(filteredNotes, req.query);
+    }
+    res.json(filteredNotes);
  }) 
  //need a post route for posting new notes
- //need a route for deleting notes
+ app.post('/api/notes', (req, res) => {
+     // set id based on what the next index of the array will be
+     console.log(notes);
+   req.body.id = notes.length.toString();
 
- app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+  if (!validateNote(req.body)) {
+    res.status(400).send('The note is not properly formatted.');
+  } else {
+    const note = makeNewNote(notes, req.body);
+    res.json(note);
+  }
+ })
+ //need a route for deleting notes
+ app.delete('/api/notes', (req, res) => {
+
  })
 
- app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-  });
+ app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+ })
+
+//  app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/index.html'));
+//   });
 
   app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
